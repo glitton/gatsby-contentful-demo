68f8e9fd190a7bea6e035fbacc72c71c7d9948c2
Generosa Litton <glcodeworks@gmail.com>
2021-04-11 22:41:09 -0700

    added text and img in about page

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -6,7 +6,7 @@ const Images = () => {
   return (
     <Wrapper>
       <article>
-        <h4>constrained / default</h4>
+        <h4>constrained</h4>
         <StaticImage
           src="../assets/images/recipe-1.jpeg"
           alt="food"
@@ -54,9 +54,13 @@ const Wrapper = styled.section`
   }
   .example-img {
     border-radius: 1rem;
+    height: 300px;
   }
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr 1fr;
+    .example-img {
+      height: 200px;
+    }
   }
 `;
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,36 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
 
 const About = () => {
   return (
     <Layout>
-      <h1>This is the About page</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>We Are All About Simplifying Recipes!</h2>
+            <p>
+              Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi
+              welsh onion daikon amaranth tatsoi tomatillo melon azuki bean
+              garlic.
+            </p>
+            <p>
+              Grape wattle seed kombu beetroot horseradish carrot squash
+              brussels sprout chard.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="person pouring salt in bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
     </Layout>
   );
 };
