81b9d077e641438aada33c5f520862f9039068e8
Generosa Litton <glcodeworks@gmail.com>
2021-04-11 22:20:22 -0700

    experimented with diff options and props

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -1,20 +1,62 @@
 import React from "react";
 import styled from "styled-components";
-import bigFood from "../assets/images/big-food.jpg";
 import { StaticImage } from "gatsby-plugin-image";
 
 const Images = () => {
   return (
     <Wrapper>
-      <img src={bigFood} alt="food" />
-      <h2>Gatsby Image</h2>
+      <article>
+        <h4>constrained / default</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          placeholder="tracedSVG"
+          layout="constrained"
+          className="example-img"
+          as="section"
+        />
+      </article>
+      <article>
+        <h4>fixed</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          placeholder="blurred"
+          layout="fixed"
+          width={200}
+          className="example-img"
+          as="div"
+        />
+      </article>
+      <article>
+        <h4>full width</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          placeholder="dominantColor"
+          layout="fullWidth"
+          className="example-img"
+          as="article"
+        />
+      </article>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.section`
-  img {
-    width: 200px;
+  width: 70vw;
+  margin: 0 auto;
+  display: grid;
+  text-align: center;
+  gap: 2rem;
+  article {
+    border: 2px solid red;
+  }
+  .example-img {
+    border-radius: 1rem;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr 1fr;
   }
 `;
 
