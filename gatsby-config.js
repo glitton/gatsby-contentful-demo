13daf51430c52c59963ef2f95e793646f7cef5f3
Generosa Litton <glcodeworks@gmail.com>
2021-04-14 16:23:59 -0700

    graphql practice files

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,18 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Recipe site",
+    originalAuthor: "@johnsmilga",
+    secondaryAuthor: "@LittonG",
+    person: { name: "John", age: 32 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "John", age: 32 },
+      { name: "Susan", age: 21 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
