72fcc025f205c9d89d55243891e64b3817f1dd40
Generosa Litton <glcodeworks@gmail.com>
2021-04-15 15:17:20 -0700

    practice with gatsby plugin image and graphql

---
+++ src/examples/Gallery.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Gallery = () => {
+  return (
+    <div>
+      <h2>Simple Image Gallery</h2>
+    </div>
+  );
+};
+
+export default Gallery;
--- src/pages/testPage.js
+++ src/pages/testPage.js
@@ -1,36 +1,17 @@
 import React from "react";
 import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Gallery from "../examples/Gallery";
+import { MainImage } from "gatsby-plugin-image";
 
 const TestPage = ({ data }) => {
-  console.log("this props", data);
-  const { secondaryAuthor, title } = data.site.info;
   return (
-    <div>
-      <h2>Testing 123</h2>
-      <h5>Title: {title}</h5>
-      <h5>Author: {secondaryAuthor}</h5>
-    </div>
+    <main className="page">
+      <Layout>
+        <Gallery />
+      </Layout>
+    </main>
   );
 };
 
-export const data = graphql`
-  {
-    site {
-      info: siteMetadata {
-        secondaryAuthor
-        description
-        simpleData
-        title
-        complexData {
-          age
-          name
-        }
-        person {
-          age
-          name
-        }
-      }
-    }
-  }
-`;
 export default TestPage;
