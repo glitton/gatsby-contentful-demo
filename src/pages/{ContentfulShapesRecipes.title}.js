395079e98379b629696e55b05ed8b9a543851b7f
Generosa Litton <glcodeworks@gmail.com>
2021-04-19 22:58:23 -0700

    fixed layout using article

--- src/pages/{ContentfulShapesRecipes.title}.js
+++ src/pages/{ContentfulShapesRecipes.title}.js
@@ -22,46 +22,46 @@ const ShapesRecipeTemplate = ({ data }) => {
       <main className="page">
         <div className="recipe-page">
           {/* hero */}
-          <section className="hero">
+          <section className="recipe-hero">
             <GatsbyImage
               image={pathToImage}
               alt={title}
               className="about-img"
             />
+            <article className="recipe-info">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              {/* icons */}
+              <div className="recipe-icons">
+                <article>
+                  <BsClock />
+                  <h5>prep time</h5>
+                  <p>{prepTime} min.</p>
+                </article>
+                <article>
+                  <BsClockHistory />
+                  <h5>cook time</h5>
+                  <p>{cookTime} min.</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>servings</h5>
+                  <p>{servings}</p>
+                </article>
+              </div>
+              {/* tags */}
+              <p className="recipe-tags">
+                Tags:
+                {tags.map((tag, index) => {
+                  return (
+                    <Link to={`/${tag}`} key={index}>
+                      {tag}
+                    </Link>
+                  );
+                })}
+              </p>
+            </article>
           </section>
-          <article className="recipe-info">
-            <h2>{title}</h2>
-            <p>{description}</p>
-            {/* icons */}
-            <div className="recipe-icons">
-              <article>
-                <BsClock />
-                <h5>prep time</h5>
-                <p>{prepTime} min.</p>
-              </article>
-              <article>
-                <BsClockHistory />
-                <h5>cook time</h5>
-                <p>{cookTime} min.</p>
-              </article>
-              <article>
-                <BsPeople />
-                <h5>servings</h5>
-                <p>{servings}</p>
-              </article>
-            </div>
-            {/* tags */}
-            <p className="recipe-tags">
-              Tags:
-              {tags.map((tag, index) => {
-                return (
-                  <Link to={`/${tag}`} key={index}>
-                    {tag}
-                  </Link>
-                );
-              })}
-            </p>
-          </article>
           {/* rest of the content */}
           <section className="recipe-content">
             <article>
