e80ddb73239e9c348a48baf88dbe901a5269e9b2
Generosa Litton <glcodeworks@gmail.com>
2021-04-21 16:51:50 -0700

    fixed nodejs setup

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 //Create tag pages programatically
->>>>>>> tag-pages
 const path = require("path");
 const slugify = require("slugify");
 exports.createPages = async ({ graphql, actions }) => {
@@ -18,20 +15,12 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `);
-<<<<<<< HEAD
-  result.data.allContentfulShapesRecipes.nodes.forEach(shapeRecipe => {
-    shapeRecipe.content.tags.forEach(tag => {
-      const tagSlug = slugify(tag, { lower: true });
-      createPage({
-        path: `/tags/${tagSlug}`,
-=======
 
   result.data.allContentfulShapesRecipes.nodes.forEach(recipe => {
     recipe.content.tags.forEach(tag => {
       const slug = slugify(tag, { lower: true });
       createPage({
         path: `/${slug}`,
->>>>>>> tag-pages
         component: path.resolve(`src/templates/tag-template.js`),
         context: {
           tag: tag,
