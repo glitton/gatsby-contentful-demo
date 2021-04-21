eb59fba6910111e47563edd34475a6a5bf947a6f
Generosa Litton <glcodeworks@gmail.com>
2021-04-21 16:42:53 -0700

    completed tag template

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../components/Layout";
 import { graphql, Link } from "gatsby";
 import setupTags from "../utils/setupTags";
+import slugify from "slugify";
 
 const Tags = ({ data }) => {
   const newTags = setupTags(data.allContentfulShapesRecipes.nodes);
@@ -12,10 +13,12 @@ const Tags = ({ data }) => {
         <section className="tags-page">
           {newTags.map((tag, index) => {
             const [text, value] = tag;
+            const tagSlug = slugify(text, { lower: true });
             return (
-              <Link to={`/${text}`} key={index} className="tag">
+              <Link to={`/${tagSlug}`} key={index} className="tag">
                 <h5>{text}</h5>
-                <p>{value} recipe</p>
+                {{ value } === 1 && <p>{value} recipe</p>}
+                <p>{value} recipes</p>
               </Link>
             );
           })}
--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -1,11 +1,38 @@
 import React from "react";
+import { graphql } from "gatsby";
+import ShapesRecipes from "../components/ShapesRecipes";
+import Layout from "../components/Layout";
 
-const TagTemplate = () => {
+const TagTemplate = ({ data, pageContext }) => {
+  const recipes = data.allContentfulShapesRecipes.nodes;
   return (
-    <div>
-      <h2>Tag template page</h2>
-    </div>
+    <Layout>
+      <main className="page">
+        <h2>{pageContext.tag}</h2>
+        <div className="tag-recipes">
+          <ShapesRecipes shapesRecipes={recipes} />
+        </div>
+      </main>
+    </Layout>
   );
 };
 
+export const query = graphql`
+  query GetRecipeByTag($tag: String) {
+    allContentfulShapesRecipes(
+      sort: { fields: title, order: ASC }
+      filter: { content: { tags: { eq: $tag } } }
+    ) {
+      nodes {
+        title
+        id
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`;
 export default TagTemplate;
