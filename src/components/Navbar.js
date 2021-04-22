6ce840154f3c92540faab63cf0547c25005c019c
Generosa Litton <glcodeworks@gmail.com>
2021-04-21 23:51:57 -0700

    Revert "removed background on logo"
    This reverts commit 903ba3f7c1e667bb8a6135157be63e4ba1c8e24c.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "gatsby";
 import { FiAlignJustify } from "react-icons/fi";
-import logo from "../assets/images/pasta-logo2.png";
+import logo from "../assets/images/pasta-logo.png";
 // logo made by monkik https://www.flaticon.com/authors/monkik
 
 const Navbar = () => {
