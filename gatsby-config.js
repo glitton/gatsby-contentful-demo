0dae5012de714163096ffdf0645d68fd9c724dd2
Generosa Litton <glcodeworks@gmail.com>
2021-04-27 16:35:57 -0700

    updated env variables and tokens in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,26 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken:
+    process.env.CONTENTFUL_ACCESS_TOKEN ||
+    process.env.CONTENTFUL_DELIVERY_TOKEN,
+};
+
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = process.env.CONTENTFUL_HOST;
+  contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+}
+
+const { spaceId, accessToken } = contentfulConfig;
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    "Contentful spaceId and the access token need to be provided."
+  );
+}
+
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -33,12 +53,7 @@ module.exports = {
     },
     {
       resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `o62tirnq6l08`,
-        // Using the Preview API Key
-        accessToken: process.env.CONTENTFUL_API_KEY,
-        host: `cdn.contentful.com`,
-      },
+      options: contentfulConfig,
     },
     {
       resolve: `gatsby-plugin-webfonts`,
