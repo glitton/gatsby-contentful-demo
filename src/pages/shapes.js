a09864622ed6203ae51292fac67c4120fc720b1e
Generosa Litton <glcodeworks@gmail.com>
2021-05-05 16:54:30 -0700

    added default to gatsby-browser.js

--- src/pages/shapes.js
+++ src/pages/shapes.js
@@ -1,16 +1,15 @@
 import React from "react";
-import Layout from "../components/Layout";
 import AllShapesRecipes from "../components/AllShapesRecipes";
 import SEO from "../components/SEO";
 
 const Shapes = () => {
   return (
-    <Layout>
+    <>
       <SEO title="Shapes" />
       <main className="page">
         <AllShapesRecipes />
       </main>
-    </Layout>
+    </>
   );
 };
 
