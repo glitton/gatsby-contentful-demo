0c006b86a02062f9ac5421720c966f10d129eff7
Generosa Litton <glcodeworks@gmail.com>
2021-07-27 14:54:30 -0500

    commented out fetch-data.js

--- src/examples/fetch-data.js
+++ src/examples/fetch-data.js
@@ -1,33 +1,30 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 
-const getData = graphql`
-  {
-    site {
-      siteMetadata {
-        originalAuthor
-        secondaryAuthor
-        complexData {
-          age
-          name
-        }
-      }
-    }
-  }
-`;
+// const getData = graphql`
+//   {
+//     site {
+//       siteMetadata {
+//         originalAuthor
+//         secondaryAuthor
+//         }
+//       }
+//     }
+//   }
+// `;
 
-const FetchData = () => {
-  const {
-    site: {
-      siteMetadata: { secondaryAuthor },
-    },
-  } = useStaticQuery(getData);
-  return (
-    <div>
-      {/* <h2>Name: {data.site.siteMetadata.secondaryAuthor}</h2> */}
-      <h2>Secondary Author: {secondaryAuthor}</h2>
-    </div>
-  );
-};
+// const FetchData = () => {
+//   const {
+//     site: {
+//       siteMetadata: { secondaryAuthor },
+//     },
+//   } = useStaticQuery(getData);
+//   return (
+//     <div>
+//       {/* <h2>Name: {data.site.siteMetadata.secondaryAuthor}</h2> */}
+//       <h2>Secondary Author: {secondaryAuthor}</h2>
+//     </div>
+//   );
+// };
 
-export default FetchData;
+// export default FetchData;
