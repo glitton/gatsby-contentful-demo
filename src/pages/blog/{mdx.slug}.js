1136d609d1690b9ad1de9fcb2e934606e7177ff2
Generosa Litton <glcodeworks@gmail.com>
2021-08-29 01:15:46 -0500

    use file system route api to dynamically create blog pages

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const BlogPost = () => {
+  return (
+    <>
+      <p>My blog post contents will go here eventually.</p>
+    </>
+  );
+};
+
+export default BlogPost;
