08e3db97d6d9984ac9bdf5de7bc2407155619a86
Generosa Litton <glcodeworks@gmail.com>
2021-11-12 10:25:08 +0800

    used gatsby image

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
   return (
     <article className="page">
       <h2>{data.mdx.frontmatter.title}</h2>
       <p>{data.mdx.frontmatter.date}</p>
-      <MDXRenderer localImage={data.mdx.frontmatter.hero_image}>
-        {data.mdx.body}
-      </MDXRenderer>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
+      <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </article>
   );
 };
@@ -18,15 +25,18 @@ export const query = graphql`
   query BlogQuery($id: String) {
     mdx(id: { ne: null, eq: $id }) {
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
+        hero_image {
+          childImageSharp {
+            gatsbyImageData(placeholder: TRACED_SVG, layout: CONSTRAINED)
+          }
+        }
         hero_image_alt
         hero_image_credit_link
         hero_image_credit_text
         title
-        hero_image
       }
-      id
       body
+      id
     }
   }
 `;
