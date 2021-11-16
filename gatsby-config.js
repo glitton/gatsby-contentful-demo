a659be8e1cde7a699a9e03b617986618123b54c8
Generosa Litton <glcodeworks@gmail.com>
2021-11-16 16:36:19 +0800

    removed gatsby plugin sharp options

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,16 +25,6 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-mdx`,
     {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        defaults: {
-          quality: 70,
-          formats: ["auto", "webp", "avif"],
-          placeholder: "blurred",
-        },
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
