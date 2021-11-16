d73df7b2b8f291b06e1cd62234cf1ec98932b147
Generosa Litton <glcodeworks@gmail.com>
2021-11-17 00:36:23 +0800

    changed hero placeholder to traced svg

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
             src="../assets/images/main.jpg"
             alt="flour and eggs"
             className="hero-img"
-            placeholder="dominantColor"
+            placeholder="tracedSVG"
             layout="fullWidth"
             loading="eager"
           />
