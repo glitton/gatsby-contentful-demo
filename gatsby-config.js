cccf93848004bd7cc9b1af0ba8ceacd8260e9150
Generosa Litton <glcodeworks@gmail.com>
2022-08-09 15:39:47 -0500

    updated contentful config

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,8 +39,8 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken:
-          process.env.CONTENTFUL_DELIVERY_TOKEN ||
-          process.env.PREVIEW_ACCESS_TOKEN,
+          process.env.CONTENTFUL_ACCESS_TOKEN ||
+          process.env.CONTENTFUL_DELIVERY_TOKEN,
         host: process.env.CONTENTFUL_HOST,
         contentTypeFilter: contentType => contentType.sys.id !== "blogs",
       },
